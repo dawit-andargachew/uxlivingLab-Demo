a227d9035c4f144fba4a017060fe2badac4596c2
Dawit Andargachew <hereisdawit@gmail.com>
2023-12-29 06:05:12 +0300

    prev button added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 export const App = () => {
   const [students, setStudents] = useState([]);
-  const [offSet, setOffSet] = useState(1);
+  const [offSet, setOffSet] = useState(0);
   const [limit, setLimit] = useState(4);
   const [loading, setLoading] = useState(false);
 
@@ -32,7 +32,31 @@ export const App = () => {
     setLoading(true);
     console.log("the function is clicked");
 
-    const nextoffSet = offSet + limit
+    const nextoffSet = offSet + limit;
+    axios
+      .get("https://100090.pythonanywhere.com/training_test/", {
+        params: {
+          type: "get_data",
+          limit: limit,
+          offset: nextoffSet,
+        },
+      })
+      .then((response) => {
+        setLoading(false);
+        setOffSet(nextoffSet);
+        setStudents(response.data.response);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log(error.message);
+      });
+  };
+
+  const handlePrev = () => {
+    setLoading(true);
+    console.log("the function is clicked");
+
+    const nextoffSet = offSet - limit;
     axios
       .get("https://100090.pythonanywhere.com/training_test/", {
         params: {
@@ -53,22 +77,30 @@ export const App = () => {
   };
 
   return (
-    <div className="my-14 outline">
+    <div className="my-14">
       {students && !loading ? (
-        <div className="w-8/12  outline mx-auto">
+        <div className="w-8/12  mx-auto">
           <ShowStudents students={students} />
         </div>
       ) : (
         <p className="text-2xl text-sky-800 text-center font-bold ">Loading</p>
       )}
 
-      <div className="flex justify-center my-5">
-        <button
+      <div className="flex justify-evenly my-5">
+        {offSet > 0 && (
+          <button
+            onClick={handlePrev}
+            className="px-8 cursor-pointer rounded-md py-2 text-white bg-sky-700"
+          >
+            Previous
+          </button>
+        )}
+       {offSet < 10 && <button
           onClick={handleNext}
-          className=" px-8 cursor-pointer outline rounded-md py-2 text-white bg-sky-700"
+          className=" px-8 cursor-pointer rounded-md py-2 text-white bg-sky-700"
         >
           Next
-        </button>
+        </button>}
       </div>
     </div>
   );
